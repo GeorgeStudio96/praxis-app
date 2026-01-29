50e068871dc9cf93dba14c5c891aeb180eebe7aa
George Ershov <geo.erh96@gmail.com>
2026-01-29 09:23:13 +0700

    Feat: Add `ArrowMedium` icon component

---
+++ src/assets/icons/ArrowMedium.vue
@@ -0,0 +1,10 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M13.3338 19L20.0005 12M20.0005 12L13.3338 5M20.0005 12L4.00049 12"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
