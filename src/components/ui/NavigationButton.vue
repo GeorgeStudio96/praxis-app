9afdf874ba70a3f3b665a50b2ba1bd0192c688fc
George Ershov <geo.erh96@gmail.com>
2026-01-29 09:23:13 +0700

    Feat: Add `NavigationButton` UI component and styles

---
+++ src/components/ui/NavigationButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import ArrowMedium from '@/assets/icons/ArrowMedium.vue';
+import IconContainer from './IconContainer.vue';
+
+interface Props {
+  direction?: 'left' | 'right';
+  size?: 'small' | 'medium' | 'large';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  direction: 'right',
+  size: 'medium',
+});
+
+const emit = defineEmits<{ click: [] }>();
+</script>
+
+<template>
+  <button @click="emit('click')" class="nav-btn">
+    <IconContainer
+      :size="24"
+      :style="props.direction === 'left' ? { transform: 'rotate(180deg)' } : {}"
+    >
+      <ArrowMedium />
+    </IconContainer>
+  </button>
+</template>
