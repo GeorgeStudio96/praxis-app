c3d6eba3e5adb2af481eee168b21e28b9356725e
George Ershov <geo.erh96@gmail.com>
2026-01-31 09:39:05 +0700

    feat: Create HeaderNav and HeaderSearch sub-components

---
+++ src/components/layout/HeaderNav.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Logo from '@/assets/icons/Logo.vue';
+import LanguageSwitcher from '@/components/ui/LanguageSwitcher.vue';
+import IconContainer from '@/components/ui/IconContainer.vue';
+import MenuIcon from '@/assets/icons/MenuIcon.vue';
+import HeaderNavShape from '@/assets/shapes/HeaderNavShape.vue';
+import HeaderMenuShape from '@/assets/shapes/HeaderMenuShape.vue';
+</script>
+
+<template>
+  <nav class="header-nav">
+    <div class="header-nav__bg">
+      <HeaderNavShape />
+    </div>
+
+    <div class="header-nav__content">
+      <a href="/" class="logo">
+        <Logo />
+      </a>
+
+      <div class="text-mini">
+        <div>Многопрофильная</div>
+        <div>группа компаний</div>
+      </div>
+
+      <div class="header-divider"></div>
+
+      <LanguageSwitcher />
+    </div>
+
+    <button class="header-menu" type="button">
+      <div class="header-menu__bg">
+        <HeaderMenuShape />
+      </div>
+      <div class="header-menu__content">
+        <IconContainer :size="40">
+          <MenuIcon />
+        </IconContainer>
+      </div>
+    </button>
+  </nav>
+</template>
---
+++ src/components/layout/HeaderSearch.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import IconContainer from '@/components/ui/IconContainer.vue';
+import IconSearch from '@/assets/icons/IconSearch.vue';
+
+const searchQuery = ref('');
+</script>
+
+<template>
+  <div class="header-search">
+    <IconContainer :size="24">
+      <IconSearch />
+    </IconContainer>
+    <input
+      v-model="searchQuery"
+      type="text"
+      class="header-search__input text-label"
+      placeholder="ПОИСК"
+    />
+  </div>
+</template>
