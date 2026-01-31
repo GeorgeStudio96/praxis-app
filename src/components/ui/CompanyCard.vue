ea64a8c945063ae50faf72ae42397dd6fbb1edf8
George Ershov <geo.erh96@gmail.com>
2026-01-31 13:28:24 +0700

    feat: Introduce CompanyCard, SliderCard components and integrate sticky layout styles

---
+++ src/components/ui/CompanyCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import ArrowRightDiagonal from '@/assets/icons/ArrowRightDiagonal.vue';
+import IconContainer from './IconContainer.vue';
+import PrintXLogo from '@/assets/icons/PrintXLogo.svg';
+
+interface Props {
+  href: string;
+  company: string;
+  subtitle?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  subtitle: 'перейти на сайт',
+});
+</script>
+
+<template>
+  <a :href="href" target="_blank" class="company-card">
+    <div class="company-card__logo">
+      <img :src="PrintXLogo" :alt="company" class="company-card__logo-image" />
+    </div>
+    <div class="company-card__content">
+      <div class="row-space-between">
+        <div class="company-card__text">
+          <p class="company-card__subtitle">{{ subtitle }}</p>
+          <p class="company-card__title">{{ company }}</p>
+        </div>
+        <IconContainer :size="24" class="company-card__arrow">
+          <ArrowRightDiagonal />
+        </IconContainer>
+      </div>
+    </div>
+  </a>
+</template>
---
+++ src/components/ui/SliderCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import NavigationButton from './NavigationButton.vue';
+
+interface Props {
+  label: string;
+  title: string;
+  description: string;
+  isCompact?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  isCompact: false,
+});
+
+const sliderCardRef = ref<HTMLDivElement>();
+
+const compactAttr = computed(() => (props.isCompact ? 'true' : 'false'));
+
+function handlePrev() {
+  alert('Prev clicked');
+}
+
+function handleNext() {
+  alert('Next clicked');
+}
+</script>
+
+<template>
+  <div class="slider-card-wrapper">
+    <NavigationButton direction="left" @click="handlePrev" />
+
+    <div ref="sliderCardRef" class="slider-card" :data-compact="compactAttr">
+      <div class="slider-card__header">
+        <p class="text-label">{{ label }}</p>
+        <h3>{{ title }}</h3>
+      </div>
+
+      <div class="slider-card__divider"></div>
+
+      <p>{{ description }}</p>
+    </div>
+
+    <NavigationButton @click="handleNext" />
+  </div>
+</template>
