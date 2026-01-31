da2d8b6bef8d066243f6c520e606d6b30d2b67fd
George Ershov <geo.erh96@gmail.com>
2026-01-31 09:39:05 +0700

    feat: Implement main Header layout component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HeaderNav from '@/components/layout/HeaderNav.vue';
+import HeaderSearch from './HeaderSearch.vue';
+</script>
+
+<template>
+  <header class="header">
+    <HeaderNav />
+    <HeaderSearch />
+  </header>
+</template>
