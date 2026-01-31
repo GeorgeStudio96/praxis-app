24d7f4f1f4652f107197ab49fc2b848409b16f26
George Ershov <geo.erh96@gmail.com>
2026-01-31 09:39:05 +0700

    feat: Add SVG shapes for Header background elements

---
+++ src/assets/shapes/HeaderNavShape.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    viewBox="0 0 504 88"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+  >
+    <foreignObject x="-25" y="-25" width="554" height="137.999">
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        style="
+          backdrop-filter: blur(12.5px);
+          clip-path: url(#bgblur_0_5070_1313_clip_path);
+          height: 100%;
+          width: 100%;
+        "
+      ></div>
+    </foreignObject>
+    <path
+      data-figma-bg-blur-radius="25"
+      d="M20.4229 4.22396L4.56583 19.5777C1.64751 22.4033 0 26.2918 0 30.3539V72.8686C0 81.1513 6.71329 87.8663 14.996 87.8685L504 87.9985V0.13251L30.8613 0.000233389C26.9654 -0.000855806 23.2218 1.5139 20.4229 4.22396Z"
+      fill="#F6F6F6"
+      fill-opacity="0.8"
+    />
+    <defs>
+      <clipPath id="bgblur_0_5070_1313_clip_path" transform="translate(25 25)">
+        <path
+          d="M20.4229 4.22396L4.56583 19.5777C1.64751 22.4033 0 26.2918 0 30.3539V72.8686C0 81.1513 6.71329 87.8663 14.996 87.8685L504 87.9985V0.13251L30.8613 0.000233389C26.9654 -0.000855806 23.2218 1.5139 20.4229 4.22396Z"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
