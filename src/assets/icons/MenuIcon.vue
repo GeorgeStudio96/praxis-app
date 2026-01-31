e2c0fa9d82f6fa214928963d0cb9cd64fc4d006b
George Ershov <geo.erh96@gmail.com>
2026-01-31 09:39:05 +0700

    feat: Add essential SVG icons for Header components

---
+++ src/assets/icons/MenuIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M33.3307 26.6673H6.66406M33.3307 20.0007H6.66406M33.3307 13.334H6.66406"
+      stroke="white"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
