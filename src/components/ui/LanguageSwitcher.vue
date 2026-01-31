83271c14a54a395cce28f6fa9aa4f00b86092542
George Ershov <geo.erh96@gmail.com>
2026-01-31 09:39:05 +0700

    feat: Implement LanguageSwitcher and extend IconContainer sizes

---
+++ src/components/ui/LanguageSwitcher.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const currentLang = ref('RU');
+
+function switchLang(lang: string) {
+  currentLang.value = lang;
+}
+</script>
+
+<template>
+  <div class="language-switcher">
+    <button
+      class="language-switcher__btn"
+      :class="{ 'language-switcher__btn--active': currentLang === 'RU' }"
+      @click="switchLang('RU')"
+    >
+      RU
+    </button>
+    <button
+      class="language-switcher__btn"
+      :class="{ 'language-switcher__btn--active': currentLang === 'EN' }"
+      @click="switchLang('EN')"
+    >
+      EN
+    </button>
+  </div>
+</template>
